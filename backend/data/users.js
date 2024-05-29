9ec85a6bd61d84f61becbd462fc8887dc8639996
Roy <roy@RoyMacBook-Pro-2.local>
2024-05-29 18:09:52 +0800

    fix user.js typo jhon -> john in user email

--- backend/data/users.js
+++ backend/data/users.js
@@ -9,7 +9,7 @@ const users = [
   },
   {
     name: "John Doe",
-    email: "jhon@email.com",
+    email: "john@email.com",
     password: bcrypt.hashSync("123456", 10),
     isAdmin: false,
   },  
