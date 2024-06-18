62e4ca18214f04d13d0bdcf7e97b3c810824df26
Roy <roy@RoyMacBook-Pro-2.local>
2024-06-18 17:16:26 +0800

    add pagination UI

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,8 +1,10 @@
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button, Row, Col } from 'react-bootstrap';
 import { FaEdit, FaTrash } from 'react-icons/fa';
+import { useParams } from 'react-router-dom';
 import Message from '../../components/Message';
 import Loader from '../../components/Loader';
+import Paginate from '../../components/Paginate';
 import {toast} from 'react-toastify';
 import { 
   useGetProductsQuery, 
@@ -12,7 +14,9 @@ import {
 
 const ProductListScreen = () => {
 
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error, refetch } = useGetProductsQuery({pageNumber,});
 
   const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
 
@@ -78,7 +82,7 @@ const ProductListScreen = () => {
               </thead>
               <tbody>
                 { 
-                  products.map((product) => (
+                  data.products.map((product) => (
                     <tr key={product._id}>
                       <td>{product._id}</td>
                       <td>{product.name}</td>
@@ -104,6 +108,7 @@ const ProductListScreen = () => {
                 }
               </tbody>
             </Table>
+            <Paginate pages={data.pages} page={data.page} isAdmin={true} />
           </>
         )
       }
