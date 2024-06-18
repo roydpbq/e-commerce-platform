7cffd9af566aaa42b85244668a8b7f81613fc2a8
Roy <roy@RoyMacBook-Pro-2.local>
2024-06-19 02:31:29 +0800

    add search UI

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -15,6 +15,11 @@ const HomeScreen = () => {
 
   return (
     <>
+      { keyword && (
+        <Link to='/' className='btn btn-light mb-4'>
+          Go Back
+        </Link>
+      )}
       { isLoading ? (
           <Loader />
       ) : error ? (
@@ -23,7 +28,7 @@ const HomeScreen = () => {
         </Message>
       ) : (
         <>
-          <h1>Latest Products</h1>
+          <h1>Latest Products {keyword && `related to "${keyword}"`}</h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
