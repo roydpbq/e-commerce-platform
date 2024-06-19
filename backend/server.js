78ac7757047e03d5256d05ff4fcb23e15861bd4a
Roy <roy@RoyMacBook-Pro-2.local>
2024-06-19 17:39:30 +0800

    add fronntend build and execution script

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,8 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Cookie parser middleware
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -37,6 +32,20 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  // any route that is not api will be directed to index.html
+  app.get('*', (req, res) => 
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
